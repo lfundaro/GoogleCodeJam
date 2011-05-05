da8c0ba0af2cfba9db2b09fa693f2ec0694f4a74
Lorenzo Fundaro <fundaro@gostai.com>
2011-05-05 13:34:02 +0200

    Added input and output processing.

--- codejam-2011/store_credit/s_sol.hs
+++ codejam-2011/store_credit/s_sol.hs
@@ -1,3 +1,5 @@
+import Data.List (genericSplitAt)
+
 type Index = Integer
 type II    = (Integer,Index)
 
@@ -27,7 +29,28 @@ disc old@(i1,i2,cr,diff) ((a,c),(b,d)) =
 credit :: Integer -> [(II,II)] -> (Index,Index,Integer,Integer)
 credit credit = foldl disc (seed credit)
 
--- main = do 
---  :( i need a monad to do this ...
-  
+-- returns problems as a tuple of items and credit
+parseCases :: [Integer] -> [([Integer],Integer)]
+parseCases (numCases:xs) = go xs
+    where 
+      go [] = []
+      go (credit:times:rest) = let (art,next) = genericSplitAt times rest
+                               in (art,credit) : go next 
+
+solve :: ([Integer],Integer) -> (Integer,Integer)
+solve (art,cr) = let (i1,i2,_,_) = credit cr (combine.index $ art)
+                 in 
+                   if i1 <= i2 then
+                       (i1+1,i2+1)
+                   else
+                       (i2+1,i1+1)
+
+main :: IO () 
+main = do 
+  file <- getContents
+  let raw = (map read (words file)) :: [Integer]
+  let cases = parseCases raw
+  let results = map solve cases
+  putStr (concatMap (\(i,n) -> "Case #" ++ show i ++ ": " ++ show n ++ "\n")
+                              (zip [1..] results))
   
